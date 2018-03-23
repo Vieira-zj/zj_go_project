8e3d755b287435e1b97392ad4de6bff43a1dc792
zhengjin <zivieira@163.com>
2018-03-23 19:24:32 +0800

    Add memcache demo.

--- src/data.db/main/main.go
+++ src/data.db/main/main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"fmt"
 
+	"data.db/memcached"
+	"data.db/mongodb"
 	"data.db/redis"
 )
 
 // build cmd: $ GOOS=linux GOARCH=amd64 go build
 // $ scp main qboxserver@10.200.20.21:~/zhengjin/main
 func main() {
+	isMongodbTest := false
+	if isMongodbTest {
+		// mongodb.ConnectToDbAndTest()
+		// mongodb.InsertRecordsToRsDb()
+		mongodb.InsertRecordsToRsDbParallel()
+	}
 
-	// mongodb.ConnectToDbAndTest()
-	// mongodb.InsertRecordsToRsDb()
-	// mongodb.InsertRecordsToRsDbParallel()
+	isRedisTest := false
+	if isRedisTest {
+		// redis.ConnectToRedisAndTest()
+		redis.MainRedis()
+	}
 
-	// redis.ConnectToRedisAndTest()
-	redis.MainRedis()
+	isMemTest := false
+	if isMemTest {
+		memcached.ConnectMemcacheAndTest()
+	}
 
 	fmt.Println("db main done.")
 }
--- src/data.db/memcached/memcached_01.go
+++ src/data.db/memcached/memcached_01.go
@@ -6,13 +6,76 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
-// ConnectMemcacheAndTest :
+// ConnectMemcacheAndTest : connect to memcached db, and test
 func ConnectMemcacheAndTest() {
-	server := "127.0.0.1:11211"
+	// create a handle
+	const server = "127.0.0.1:11211"
 	mc := memcache.New(server)
 	if mc == nil {
 		fmt.Println("memcache New failed")
 	}
-	// TODO:
-	// https://blog.csdn.net/weixin_37696997/article/details/78760397
+
+	mc.Add(&memcache.Item{Key: "foo", Value: []byte("my value")})
+
+	it, err := mc.Get("foo")
+	if err != nil {
+		fmt.Println("get failed, error:", err.Error())
+	}
+	if string(it.Key) == "foo" {
+		fmt.Println("value is:", string(it.Value))
+	}
+
+	mc.Set(&memcache.Item{Key: "foo", Value: []byte("bluego")})
+	it, err = mc.Get("foo")
+	if err != nil {
+		fmt.Println("add failed, error:", err.Error())
+	}
+	if string(it.Key) == "foo" {
+		fmt.Println("value is:", string(it.Value))
+	}
+
+	// mc.Replace(&memcache.Item{Key: "foo", Value: []byte("mobike")})
+	// it, err = mc.Get("foo")
+	// if err != nil {
+	// 	fmt.Println("replace failed, error:", err.Error())
+	// }
+	// if string(it.Key) == "foo" {
+	// 	fmt.Println("value is:", string(it.Value))
+	// }
+
+	// delete an exist key
+	err = mc.Delete("foo")
+	if err != nil {
+		fmt.Println("delete failed, error:", err.Error())
+	} else {
+		fmt.Println("delete success.")
+	}
+
+	// incrby
+	err = mc.Set(&memcache.Item{Key: "myint", Value: []byte("1")})
+	if err != nil {
+		fmt.Println("set failed, error:", err.Error())
+	}
+	value, err := mc.Increment("myint", 7)
+	if err != nil {
+		fmt.Println("increment failed, error:", err.Error())
+	}
+	if string(it.Key) == "foo" {
+		fmt.Println("after increment the value is:", value)
+	}
+
+	// decrby
+	value, err = mc.Decrement("myint", 4)
+	if err != nil {
+		fmt.Println("decrement failed", err.Error())
+	} else {
+		fmt.Println("after decrement the value is:", value)
+	}
+
+	err = mc.Delete("myint")
+	if err != nil {
+		fmt.Println("delete failed, error:", err.Error())
+	} else {
+		fmt.Println("delete success.")
+	}
 }
