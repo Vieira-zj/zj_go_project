190045527daf14819d5f50aa4690df97977fe67a
zhengjin <zivieira@163.com>
2020-02-25 22:09:29 +0800

    Add leetcode samplers.

--- src/demo.hello/sort/leetcode_3.go
+++ src/demo.hello/sort/leetcode_3.go
@@ -165,8 +165,54 @@ func isPalindromeLinkedList(head *listNode) bool {
 // ------------------------------
 
 func getKthFromEnd(head *listNode, k int) *listNode {
-	// TODO:
-	return nil
+	slow := head
+	fast := head
+
+	for i := 0; i < k; i++ {
+		fast = fast.Next
+	}
+
+	for fast != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
+
+// ------------------------------
+// 6. 替换空格
+// 时间复杂度：O(n) 空间复杂度：O(n)
+// ------------------------------
+
+func replaceSpace(s string) string {
+	ret := make([]rune, 0, len(s)*3)
+	for _, r := range s {
+		if r == ' ' {
+			ret = append(ret, '%', '2', '0')
+		} else {
+			ret = append(ret, r)
+		}
+	}
+	return string(ret)
+}
+
+// ------------------------------
+// 7. 左旋转字符串
+// 把字符串前面的若干个字符转移到字符串的尾部。
+// ------------------------------
+
+func reverseLeftWords01(s string, n int) string {
+	b := []rune(s)
+	for i := 0; i < n; i++ {
+		for j := 0; j < len(b)-1; j++ {
+			b[j], b[j+1] = b[j+1], b[j]
+		}
+	}
+	return string(b)
+}
+
+func reverseLeftWords02(s string, n int) string {
+	return s[n:] + s[:n]
 }
 
 // LeetCodeMain03 contains leetcode algorithms.
@@ -176,25 +222,43 @@ func LeetCodeMain03() {
 		fmt.Println("expect true, and actual:", containsDuplicate([]int{1, 2, 3, 1}))
 		fmt.Println("expect false, and actual:", containsDuplicate([]int{1, 2, 3, 4}))
 
-		fmt.Println("\n#2.1 删除排序数组中的重复项")
+		fmt.Println("\n#2. 删除排序数组中的重复项")
+		fmt.Println("#2.1")
 		fmt.Println("expect 2, and actual:", removeDuplicates01([]int{1, 1, 2}))
 		fmt.Println("expect 5, and actual:", removeDuplicates01([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
-		fmt.Println("\n#2.2 删除排序数组中的重复项")
+		fmt.Println("#2.2")
 		fmt.Println("expect 2, and actual:", removeDuplicates02([]int{1, 1, 2}))
 		fmt.Println("expect 5, and actual:", removeDuplicates02([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
 
-		fmt.Println("\n#3.1 缺失数字")
+		fmt.Println("\n#3. 缺失数字")
+		fmt.Println("#3.1")
 		fmt.Println("expect 2, and actual:", missingNumber01([]int{3, 0, 1}))
 		fmt.Println("expect 8, and actual:", missingNumber01([]int{9, 6, 4, 2, 3, 5, 7, 0, 1}))
-		fmt.Println("\n#3.2 缺失数字")
+		fmt.Println("#3.2")
 		fmt.Println("expect 2, and actual:", missingNumber02([]int{3, 0, 1}))
 		fmt.Println("expect 8, and actual:", missingNumber02([]int{9, 6, 4, 2, 3, 5, 7, 0, 1}))
 
-		fmt.Println("\n#4 回文链表")
+		fmt.Println("\n#4. 回文链表")
 		list1 := createListNodes([]int{1, 2})
 		fmt.Println("expect false, and actual:", isPalindromeLinkedList(list1))
 		list2 := createListNodes([]int{1, 2, 2, 1})
 		fmt.Println("expect true, and actual:", isPalindromeLinkedList(list2))
+
+		fmt.Println("\n#5. 链表中倒数第k个节点")
+		list3 := createListNodes([]int{1, 2, 3, 4, 5})
+		fmt.Println("expect [4,5], and actual:")
+		printListNodes(getKthFromEnd(list3, 2))
+
+		fmt.Println("\n#6. 替换空格")
+		fmt.Println("expect 'We%20are%20happy.', and actual:", replaceSpace("We are happy."))
+
+		fmt.Println("\n#7. 左旋转字符串")
+		fmt.Println("#7.1")
+		fmt.Println("expect 'cdefgab', and actual:", reverseLeftWords01("abcdefg", 2))
+		fmt.Println("expect 'umghlrlose', and actual:", reverseLeftWords01("lrloseumgh", 6))
+		fmt.Println("#7.2")
+		fmt.Println("expect 'cdefgab', and actual:", reverseLeftWords02("abcdefg", 2))
+		fmt.Println("expect 'umghlrlose', and actual:", reverseLeftWords02("lrloseumgh", 6))
 	}
 
 	fmt.Println("leetcode sample3 done.")
