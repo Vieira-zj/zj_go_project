66cc80ebf33161c488a717d9ff5e33631621c91e
zhengjin <zivieira@163.com>
2020-04-10 17:04:42 +0800

    Fix build.

--- src/tools.app/apps/webhooks/build.sh
+++ src/tools.app/apps/webhooks/build.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+set -u
 
-echo "build admission-webhook-example bin"
-# dep ensure -v
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o admission-webhook-example
+DOCKER_USER="zhengjin"
 
-echo "build admission-webhook-example image"
-: ${DOCKER_USER:? required}
-docker build --no-cache -t ${DOCKER_USER}/admission-webhook-example:v1 .
-# docker push ${DOCKER_USER}/admission-webhook-example:v1
+function build_bin() {
+  echo "build admission-webhook-example bin"
+  #dep ensure -v
+  CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o admission-webhook-example
+}
 
-rm -rf admission-webhook-example
+function build_image() {
+  bin_file="admission-webhook-example"
+  if [[ ! -f ${bin_file} ]]; then
+    echo "go bin file ${bin_file} not exist."
+    exit -1
+  fi
+  
+  echo "build admission-webhook-example image"
+  : ${DOCKER_USER:? required}
+  docker build --no-cache -t ${DOCKER_USER}/admission-webhook-example:v1 .
+  #docker push ${DOCKER_USER}/admission-webhook-example:v1
+
+  #rm -rf ${bin_file} 
+}
+
+if [[ $1 == "bin" ]]; then
+  build_bin
+fi
+
+if [[ $1 = "image" ]]; then
+  build_image
+fi
+
 echo "build done"
